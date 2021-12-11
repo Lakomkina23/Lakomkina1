a4c1d14061c2913398c2810ffc6c89eb99f05e15
Lak <lakomkina23@mail.ru>
2021-12-11 10:58:40 +0300

    massive

---
+++ array.php
@@ -0,0 +1,45 @@
+<?php
+
+$numbers = [1,2,3,4,5];
+
+echo $numbers [2];
+echo '<br>';
+
+$count = count ($numbers);
+for ($i = 0; $i < $count ; $i ++) {
+    echo '$i $numbers [$i] <br>';
+}
+
+echo '<hr>';
+
+foreach ($numbers as $key => $value) {
+    echo '$key; $value <br>';
+}
+
+
+$new_numbers = [];
+for ($i =0; $i<10; $i++) {
+    $new_numbers []= $i**2;
+}
+
+$new_numbers [10] = 100;
+
+echo '<pre>';
+print_r ($new_numbers);
+
+$person = [
+    'name' => 'Iullia',
+    'age' => 37,
+    'city'=> 'Moscow'
+];
+
+echo $person ['name'];
+
+echo '<pre>';
+print_r ($person);
+
+
+unset ($person ['name']);
+
+echo '<pre>';
+print_r ($person);
